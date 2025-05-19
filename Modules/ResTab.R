2336f382d3885f8f13c9c1c7ff9d9d541bc32fcf
drclongstaff <drclongstaff@gmail.com>
2025-05-19 16:26:29 +0100

    Adjust styling

--- Modules/ResTab.R
+++ Modules/ResTab.R
@@ -1,28 +1,21 @@
 myRes <- read.csv("Data/myRes.csv")
 
-filenames_ui <- function(id){
+filenames_ui <- function(id) {
   ns <- NS(id)
-  
+
   tagList(
-    #dataTableOutput(ns("obs"))
+    # dataTableOutput(ns("obs"))
     DT::DTOutput(ns("obs"))
   )
-  
 }
 
-filenames_server <- function(id, theobs){
-  moduleServer(id, function(input, output, session){
-    
+filenames_server <- function(id, theobs) {
+  moduleServer(id, function(input, output, session) {
     myobs <- reactive({
-      
-      AA <- matrix(theobs(), byrow=TRUE, nrow = input$numrows)
+      AA <- matrix(theobs(), byrow = TRUE, nrow = input$numrows)
       AA <- data.frame(AA)
       setNames(AA, rep(" ", length(AA)))
-      #clipr::write_clip(AA)
+      # clipr::write_clip(AA)
     })
-    
   })
-  
 }
-
-
