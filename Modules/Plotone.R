95035f7d3cc67844f17e717c020f9b68785a2055
drclongstaff <drclongstaff@gmail.com>
2025-05-19 16:25:52 +0100

    Adjust syling

--- Modules/Plotone.R
+++ Modules/Plotone.R
@@ -1,13 +1,12 @@
-
 # This function plots data from all the wells
 one_plotFun <- function(PLATE, WELLNUM, TABRES) {
   # Set plotting parameters
   TabRes <- TABRES
-  
+
   k <- WELLNUM
-  
+
   par(mar = c(4, 4, 1, 1)) # dimensions for figure
-  
+
   # Only one plot in this case so no looping needed
   plotmake_fun(PLATE, Time, TabRes, mint, maxt, maxy, samples, k)
 }
@@ -17,31 +16,33 @@ one_plotFun <- function(PLATE, WELLNUM, TABRES) {
 library(shiny)
 # plot module ----
 oneplot_ui <- function(id) {
- 
   ns <- NS(id)
-  
-    card(
-      height = 450,
-      full_screen = TRUE,
-      card_header(numericInput(ns("wellnum"),label = "Plot well number", value = 1, min = 1, step = 1),
-                  class = "bg-info"),
-      card_body(
-        style = "background-color: #FAFBFB ;",
-    tagList(
-    plotOutput(ns("oneplot"))
-          )
-        )
+
+  card(
+    height = 450,
+    full_screen = TRUE,
+    card_header(numericInput(ns("wellnum"), label = "Plot well number", value = 1, min = 1, step = 1),
+      class = "bg-info"
+    ),
+    card_body(
+      style = "background-color: #FAFBFB ;",
+      tagList(
+        plotOutput(ns("oneplot"))
       )
-  }
+    )
+  )
+}
 
-  oneplot_server <- function(id, procfile, resfile) {#use df or procfile here or resfile or myRes?
-   
-      moduleServer(id, function(input, output, session) {
-      #thenames <- reactive({colnames(procfile()[,-1])}) #may try to use names of wells and selectize
-      #aname <- reactive({thenames()[input$wellnum]})
-      plot <- reactive({one_plotFun(procfile(), input$wellnum, resfile())})
-      output$oneplot <- renderPlot({plot()})
-    
-      })
-  }
+oneplot_server <- function(id, procfile, resfile) { # use df or procfile here or resfile or myRes?
 
+  moduleServer(id, function(input, output, session) {
+    # thenames <- reactive({colnames(procfile()[,-1])}) #may try to use names of wells and selectize
+    # aname <- reactive({thenames()[input$wellnum]})
+    plot <- reactive({
+      one_plotFun(procfile(), input$wellnum, resfile())
+    })
+    output$oneplot <- renderPlot({
+      plot()
+    })
+  })
+}
