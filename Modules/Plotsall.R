b52447726cb07f72f29e78c0ff68a0aca6cfbcfb
drclongstaff <drclongstaff@gmail.com>
2025-05-19 16:25:12 +0100

    Adjust styling

--- Modules/Plotsall.R
+++ Modules/Plotsall.R
@@ -1,18 +1,17 @@
-
 multi_plotFun <- function(PLATE, ROWNUM, TABRES) {
   # Set up some plotting parameters
   TabRes <- TABRES
   RowNum <- ROWNUM
-  
+
   plateData <- PLATE[, -1]
   absWells <- length(plateData[1, ])
   par(mfrow = c(RowNum, (absWells / RowNum))) # Organisation of multiple plots
   par(mar = c(0.2, 0.2, 0.2, 0.2)) # Dimensions for figure
-  
+
   # Generate the plots from plotmake_fun
   lapply(seq_along(plateData), function(k) {
     # for(k in seq_along(plateData)){ #This is an alternative loop
-    
+
     plotmake_fun(PLATE, Time, TabRes, mint, maxt, maxy, samples, k)
   }) # remove this ')' with alternative loop
 }
@@ -22,30 +21,30 @@ library(shiny)
 # plot module ----
 
 plot_ui <- function(id) {
- 
-      ns <- NS(id)
-            card(
-                height = 450,
-                full_screen = TRUE, #is expandable
-                  card_header(numericInput(ns("numrows"),label = "Plot number of rows", value = 3, min = 1, step = 1),
-                              class = "bg-info"),
-                  card_body(
-                      style = "background-color: #FAFBFB  ;",
-            tagList(
-        
-              plotOutput(ns("plot"))
-              
-                  )
-            )
+  ns <- NS(id)
+  card(
+    height = 450,
+    full_screen = TRUE, # is expandable
+    card_header(numericInput(ns("numrows"), label = "Plot number of rows", value = 3, min = 1, step = 1),
+      class = "bg-info"
+    ),
+    card_body(
+      style = "background-color: #FAFBFB  ;",
+      tagList(
+        plotOutput(ns("plot"))
       )
-  }
+    )
+  )
+}
 
-plot_server <- function(id, procfile, resfile) {#use df or procfile here or resfile or myRes?
-    
-      moduleServer(id, function(input, output, session) {
-        plot <- reactive({multi_plotFun(procfile(), input$numrows, resfile())})
-        output$plot <- renderPlot({plot()})
-    
-      })
-  }
+plot_server <- function(id, procfile, resfile) { # use df or procfile here or resfile or myRes?
 
+  moduleServer(id, function(input, output, session) {
+    plot <- reactive({
+      multi_plotFun(procfile(), input$numrows, resfile())
+    })
+    output$plot <- renderPlot({
+      plot()
+    })
+  })
+}
