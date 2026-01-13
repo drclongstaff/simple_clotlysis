4ba8c7d471106c58895377b35e08df3f6d8dfbff
drclongstaff <drclongstaff@gmail.com>
2026-01-13 17:17:49 +0000

    Tidy up plotting functions

--- Modules/Plotsall.R
+++ Modules/Plotsall.R
@@ -1,20 +1,18 @@
 multi_plotFun <- function(PLATE, ROWNUM, TABRES) {
   # Set up some plotting parameters
-  TabRes <- TABRES
-  RowNum <- ROWNUM
+  #TabRes <- TABRES
+  #RowNum <- ROWNUM
 
   plateData <- PLATE[, -1]
   absWells <- length(plateData[1, ])
   maxy <- max(plateData, na.rm = TRUE)*1.1 # To set the maximum of the whole plate
-  par(mfrow = c(RowNum, (absWells / RowNum))) # Organisation of multiple plots
+  par(mfrow = c(ROWNUM, (absWells / ROWNUM))) # Organisation of multiple plots
   par(mar = c(0.2, 0.2, 0.2, 0.2)) # Dimensions for figure
 
   # Generate the plots from plotmake_fun
   lapply(seq_along(plateData), function(k) {
-    # for(k in seq_along(plateData)){ #This is an alternative loop
-
-    plotmake_fun(PLATE, TabRes, k, axx="n", axy="n", maxy)
-  }) # remove this ')' with alternative loop
+    plotmake_fun(PLATE, TABRES, k, axx="n", axy="n", maxy)
+  }) 
 }
 
 
